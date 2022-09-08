19947037995175ef5a3cb1d7b17f351746995469
Princess <egbebuprincess@gmail.com>
2022-09-08 14:29:44 -0700

    k

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 #include<stdio.h>
-   /*prints the given string*/
+/*prints the given string*/
 int main(void)
 {
   puts("Programming is like building a multilingual puzzle\n");
   return 0
 }
+gcc
