d643359b5d2b8399cef1da052bbbc099201a2d33
Princess <egbebuprincess@gmail.com>
2022-09-08 14:19:09 -0700

    Made an edit to 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,8 +3,7 @@
 #include<stdio.h)
    /*prints the given string*/
 int main()
-  int puts(void)
-  {
+{
   fwrite("Programming is like building a multilingual puzzle\n");
-    return 0
-  }
+  return 0
+}
