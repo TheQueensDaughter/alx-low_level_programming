49007b80cd57d52c6bb30b7d7d13594734689c79
Princess <egbebuprincess@gmail.com>
2022-09-08 15:54:22 -0700

    Added a file that prints the sizes of data types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *Return: 0(Success)
+ */
+
+int main(void)
+{
+printf("size of char is=%i byte\n" , sizeof(char));
+printf("size of int is=%i byte\n" , sizeof(int));
+printf("size of long int is=% byte\n" , sizeof(long int));
+printf("size of long long int is%= byte\n" , sizeof(long long int));
+printf("size of float is%= byte\n" , sizeof(float));
+return (0);
+}
