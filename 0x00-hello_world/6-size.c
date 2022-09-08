b67c8d6c2a34a1380df79a7b5898c44bcfe9efae
Princess <egbebuprincess@gmail.com>
2022-09-08 15:57:41 -0700

    édit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,10 +8,10 @@
 
 int main(void)
 {
-printf("size of char is=%i byte\n" , sizeof(char));
-printf("size of int is=%i byte\n" , sizeof(int));
-printf("size of long int is=% byte\n" , sizeof(long int));
-printf("size of long long int is%= byte\n" , sizeof(long long int));
-printf("size of float is%= byte\n" , sizeof(float));
+printf("size of char is=%i byte\n", sizeof(char));
+printf("size of int is=%i byte\n", sizeof(int));
+printf("size of long int is=%i byte\n", sizeof(long int));
+printf("size of long long int is=%i byte\n", sizeof(long long int));
+printf("size of float is=%i byte\n", sizeof(float));
 return (0);
 }
