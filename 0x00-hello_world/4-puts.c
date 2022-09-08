0ebbc2e7cdb54e9bd4c38407304813a558cf463a
Princess <egbebuprincess@gmail.com>
2022-09-08 14:54:15 -0700

    edit to 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
 /*prints the given string*/
 int main(void)
 {
-  puts("Programming is like building a multilingual puzzle\n");
-  return 0;
+  puts("\"Programming is like building a multilingual puzzle\n");
+  return (0);
 }
 
