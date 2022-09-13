f48b9cf06c4f53655cf0d7013f8f49a490bdce8f
Princess <egbebuprincess@gmail.com>
2022-09-12 23:58:31 -0700

    edited task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,9 +12,9 @@ int x;
 int y;
 for (x = '0'; x <= '9'; x++)
 {
-for (y = '0'; y <= '9'; y++)
+for (y = x; y <= '9'; y++)
 {
-if (x < y)
+if (x != y)
 putchar(x);
 putchar(y);
 if (x != '8' && y != '9')
