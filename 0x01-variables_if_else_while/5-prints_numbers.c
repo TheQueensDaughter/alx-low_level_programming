e5a650006fc583c5d6933f63602b0c5319be5487
Princess <egbebuprincess@gmail.com>
2022-09-12 22:11:59 -0700

    added task 5

---
+++ 0x01-variables_if_else_while/5-prints_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio>
+
+/**
+ *main- prints numers 0-9
+ *
+ *Return: 0(Success)
+ */
+
+int main(void)
+{
+int x;
+for (x = 0; x <= 10; x++)
+{
+printf("%i", x);
+}
+putchar('\n');
+return (0);
+}
