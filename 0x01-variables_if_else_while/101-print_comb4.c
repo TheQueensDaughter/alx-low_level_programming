a1d15408bb18556ded5ed58f30f8b8ccb4253f69
Princess <egbebuprincess@gmail.com>
2022-09-13 01:21:31 -0700

    added task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -3,4 +3,28 @@
 /**
  *main- Prints all possible different combinations of three digits
  *
- *Return: 
+ *Return: 0(Success)
+ */
+
+int main(void)
+{
+int x;
+int y;
+int z;
+for (x = '0'; x <= '9'; x++)
+{
+for (y = x + 1; y <= '9'; y++)
+{
+for (z = y + 1; z <= '9'; z++)
+{
+putchar(x);
+putchar(y);
+putchar(z);
+if (x != '7')
+putchar(',');
+putchar(' ');
+}
+}
+}
+return (0);
+}
