eedfdea0d6e2d77f5bffcf8b74a7e82337a10b4e
Princess <egbebuprincess@gmail.com>
2022-09-13 19:20:38 -0700

    Added Task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *_isalpha- function that determines if a char is 
+ *an alphabet or not
+ *@c: the variable to be accessed as argument
+ *Return: 1(Success) 0(Not)
+ */
+
+int _isalpha(int c)
+{
+if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+return (1);
+else
+return (0);
+}
