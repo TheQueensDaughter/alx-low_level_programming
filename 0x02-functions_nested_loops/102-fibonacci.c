b101207cef1cb1b50166a31dafd709f03ff770aa
Princess <egbebuprincess@gmail.com>
2022-09-13 21:40:29 -0700

    Added Task 14

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,18 @@
+#input <stdio.h>
+
+/**
+ *main- prints the first 50 fibonacci numbers
+ *
+ */
+
+int main(void)
+{
+int i;
+int num1 = 1;
+for (i = 1; i < 50; i++)
+{
+printf("%i, ", num1);
+num1 += num1
+}
+printf("%i\n", num1);
+}
