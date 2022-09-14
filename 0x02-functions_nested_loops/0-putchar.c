5dfed31ed83bb1c912e50729450e0057c5411e5a
Princess <egbebuprincess@gmail.com>
2022-09-13 17:06:38 -0700

    Added new directory and file containing work on Funtions and nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ *main- Prints _putchar followed by a new line
+ *
+ *Returns 0(Success)
+ */
+
+int main(void)
+{
+printf("_putchar\n");
+return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This is a directory on functions and nested loops
