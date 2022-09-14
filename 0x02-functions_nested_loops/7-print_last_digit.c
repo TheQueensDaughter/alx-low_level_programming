ba43abb8db5e3e4ca03ab97f5173c3fc3623bcae
Princess <egbebuprincess@gmail.com>
2022-09-13 20:09:21 -0700

    Added Task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *print_last_digit- Prints last digit of a number
+ *@c: variable to be accessed as argument
+ *Return: value of last digit
+ */
+
+int print_last_digit(int c)
+{
+i = c % 10;
+return (i);
+}
