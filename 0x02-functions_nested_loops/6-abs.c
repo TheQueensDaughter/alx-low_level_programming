5f22f542d181a65119e3ae5c98351d46eea99048
Princess <egbebuprincess@gmail.com>
2022-09-13 20:01:17 -0700

    Added Task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *_abs- function that prints absolut value of a number
+ *@c: the variable that is accessed as the argument
+ *Return:c(positive) -c(negative)
+ */
+
+int _abs(int c)
+{
+if (c >= 0)
+{
+return (c);
+}
+else
+{
+return (c * -1);
+}
+}
