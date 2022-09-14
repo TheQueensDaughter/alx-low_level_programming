c1d2776a0d9691003b0ba483b6ff24405e04604b
Princess <egbebuprincess@gmail.com>
2022-09-13 17:42:44 -0700

    Added Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *main- Prints lowercase alphabets 
+ *but not with the standard library
+ *Return: 0(Success)
+ */
+
+int main(void)
+{
+void print_alphabet(void);{
+int x;
+for int(x = 'a'; x <= 'z'; x++)
+{
+_putchar(x)
+return;
+}
+}
+return (0);
+}
