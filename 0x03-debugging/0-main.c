94f6f2e923d94eb9c3d0a8feb2a3e3fd2cb11a80
Princess <egbebuprincess@gmail.com>
2022-09-14 20:12:32 -0700

    Added Task 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *main- tests a function that prints if an interger is 
+ *positive or negative
+ *Return: (0)Success
+ */
+
+int main(void)
+{
+int i;
+
+i = 0;
+positive_or_negative(i);
+
+return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+This is the directory for projects on Debugging
