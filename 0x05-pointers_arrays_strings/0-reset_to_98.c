141b00e9801d33297ae038367789e30ca091e748
Princess <egbebuprincess@gmail.com>
2022-09-23 15:34:40 -0700

    Edited Task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,7 +3,7 @@
 /**
  *reset_to_98- takes a pointer and updates the value it
  *points to
- *n- The pointer that points to an int value
+ *n: The pointer that points to an int value
  */
 
 void reset_to_98(int *n)
