bfd27537ea3367b43a76b037b079a3232a4d3ca4
Princess <egbebuprincess@gmail.com>
2022-09-24 16:20:37 -0700

    Edited Task 1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -3,8 +3,8 @@
 /**
  *swap_int- swaps the values of two intergers using
  *pointers
- *@a- the first argument of the function
- *@b- the second argument of the function
+ *@a: the first argument of the function
+ *@b: the second argument of the function
  */
 
 void swap_int(int *a, int *b)
