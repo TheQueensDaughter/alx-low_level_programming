d766636c9aa27b13a6364700951a0449a94809ab
Princess <egbebuprincess@gmail.com>
2022-09-26 14:47:22 -0700

    Added Task 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,9 +7,21 @@
 
 void rev_string(char *s)
 {
-  int i, n = _strlen(*s);
-  for (i = 0, i < n, i++)
+  int i, n;
+  char *pntr, temp[n];
+  pntr = s;
+  while (*pntr != '\0')
     {
-      swap(*(s + i), *(s + (n - i - 1)));
+      pntr++;
+      n++;
     }
+  for (i = 0; i < n; i++)
+    {
+      temp [i] = *(s + n - 1 -i);
+    }
+  i = 0;
+  for (i = 0; i < n; i++)
+    {
+      *(s + i) = temp[i];
+}
     }
