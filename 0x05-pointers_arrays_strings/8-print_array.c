d956ef3ef7137a46c9f2636d523e6bf560b805aa
Princess <egbebuprincess@gmail.com>
2022-09-26 09:07:30 -0700

    Added Task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ *print_array- Prints n elements of an array in order
+ *@a: Pointer to the first element of the array
+ *@n: Number of elements to be printed
+ */
+
+void print_array(int *a, int n)
+{
+int i;
+while (i < n)
+{
+if (i < n -1)
+{
+printf("%i, ", *(a + i));
+}
+else
+{
+printf("%i", *(a + i));
+}
+i++;
+}
+putchar('\n');
+}
