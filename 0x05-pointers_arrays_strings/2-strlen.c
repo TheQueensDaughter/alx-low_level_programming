198dfd8e9989c69780df9760c3a7197fb7295120
Princess <egbebuprincess@gmail.com>
2022-09-26 06:07:34 -0700

    Edited Task 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 
 /**
- *_strlen- prints tge length of a string
+ *_strlen- prints the length of a string
  *@s: The pointer to string to be considered
+ *Return: the length of the string
  */
 
 int _strlen(char *s)
 {
 int c = 0;
-while(*s != '\0')
+while (*s != '\0')
 {
 s++;
 c++;
